fc6f4118e8822641ec26b9fbf5adf7ce3ce188e4
Rshalika <rshal13@freeuni.edu.ge>
2016-06-10 01:51:50 +0400

    update API for changing schedule * implement basic elements according to API

--- src/main/java/ge/edu/freeuni/sdp/sprinklerscheduler/core/SchedulerStatus.java
+++ src/main/java/ge/edu/freeuni/sdp/sprinklerscheduler/core/SchedulerStatus.java
@@ -1,11 +1,15 @@
 package ge.edu.freeuni.sdp.sprinklerscheduler.core;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import ge.edu.freeuni.sdp.sprinklerscheduler.core.shchedule.Day;
+import ge.edu.freeuni.sdp.sprinklerscheduler.core.shchedule.Schedule;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 
 /**
  * Created by GM on 6/9/2016.
@@ -28,4 +32,45 @@ public class SchedulerStatus {
     public Response root() {
         return Response.ok().build();
     }
+
+
+    /**
+     * @return current Schedule
+     */
+    @Path("/schedule")
+    @GET
+    public Schedule schedule() {
+        return Schedule.getInstance();
+    }
+
+
+    /**
+     * @param schedule new schedule for scheduler
+     * @return response code 200 : schedule changed
+     */
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/schedule")
+    @POST
+    public Response setSchedule(Schedule schedule) {
+        Schedule schedule1 = Schedule.getInstance();
+        schedule1.setEndMonth(schedule.getEndMonth());
+        schedule1.setStartMonth(schedule.getStartMonth());
+        schedule1.setExcluded(schedule.getExcluded());
+
+        return Response.ok().build();
+    }
+
+
+    /**
+     * @param day new excluded day
+     * @return response code 200: excluded day added
+     */
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/schedule/excluded")
+    @POST
+    public Response addExcluded(Day day){
+        Schedule.getInstance().addExcluded(day);
+        return Response.ok().build();
+    }
+
 }
