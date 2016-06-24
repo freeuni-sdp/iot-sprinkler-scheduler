c075e5b5886cd5dda705aa4ed5e53da494dda4dd
Rshalika <rshal13@freeuni.edu.ge>
2016-06-25 00:08:19 +0400

    recreate listener comment bad test

---
+++ src/main/java/Listener.java
@@ -0,0 +1,76 @@
+
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import ge.edu.freeuni.sdp.iot.service.scheduler.sprinkler.shchedule.data.Schedule;
+import ge.edu.freeuni.sdp.iot.service.scheduler.sprinkler.shchedule.updater.SprinklerStatusUpdater;
+import ge.edu.freeuni.sdp.iot.service.scheduler.sprinkler.sprinkler.comunicator.SprinklerCommunicatorProxy;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+
+@WebListener()
+public class Listener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    // Public constructor is required by servlet spec
+    public Listener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("listener started");
+        new SprinklerStatusUpdater(Schedule.getInstance(),new SprinklerCommunicatorProxy()).start();
+
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    public void sessionCreated(HttpSessionEvent se) {
+      /* Session is created. */
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+      /* Session is destroyed. */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute 
+         is added to a session.
+      */
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attibute
+         is replaced in a session.
+      */
+    }
+}
