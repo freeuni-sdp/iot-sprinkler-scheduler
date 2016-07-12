df6802e4dd5113087e0abeaac51db4dba6e21bdc
Rshalika <rshal13@freeuni.edu.ge>
2016-07-12 15:31:15 +0400

    remove extra class

---
+++ test/ge/edu/freeuni/sdp/iot/service/scheduler/sprinkler/SchedulerServiceTest.java
@@ -0,0 +1,36 @@
+package ge.edu.freeuni.sdp.iot.service.scheduler.sprinkler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rezo on 7/12/16.
+ */
+public class SchedulerServiceTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void root() throws Exception {
+
+    }
+
+    @Test
+    public void schedule() throws Exception {
+
+    }
+
+    @Test
+    public void setSchedule() throws Exception {
+
+    }
+
+    @Test
+    public void addExcluded() throws Exception {
+
+    }
+
+}
